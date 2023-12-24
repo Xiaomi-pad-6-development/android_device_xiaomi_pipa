0bee5fb215890e657084ecab95dd9333bf9f63b8
DragonEmperor9480 <amruteshnaregal1234@gmail.com>
2023-12-24 20:46:12 +0530

    pipa: update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,17 +1,17 @@
 echo 'cloning common tree'
-git clone https://github.com/DragonEmperor9480/android_device_xiaomi_sm8250-common.git device/xiaomi/sm8250-common
+git clone https://github.com/Xiaomi-pad-6-development/android_device_xiaomi_sm8250-common.git device/xiaomi/sm8250-common
 
 echo 'Cloning proprietary vendor'
-git clone https://github.com/DragonEmperor9480/proprietary_vendor_xiaomi_pipa.git vendor/xiaomi/pipa
+git clone https://github.com/Xiaomi-pad-6-development/proprietary_vendor_xiaomi_pipa.git vendor/xiaomi/pipa
 
 echo 'cloning common vendor'
-git clone https://github.com/DragonEmperor9480/proprietary_vendor_xiaomi_sm8250-common.git vendor/xiaomi/sm8250-common
+git clone https://github.com/Xiaomi-pad-6-development/proprietary_vendor_xiaomi_sm8250-common.git vendor/xiaomi/sm8250-common
 
 echo 'cloning kernel'
-git clone https://github.com/DragonEmperor9480/android_kernel_xiaomi_sm8250.git kernel/xiaomi/sm8250 --depth=1
+git clone https://github.com/crdroidandroid/android_kernel_xiaomi_sm8250.git -b 14.0-pipa kernel/xiaomi/sm8250 --depth=1
 
 echo 'cloning xiaomi hals'
-git clone https://github.com/DragonEmperor9480/android_hardware_xiaomi.git hardware/xiaomi
+git clone https://github.com/Xiaomi-pad-6-development/android_hardware_xiaomi.git hardware/xiaomi
 
 echo 'removing vendorsetup.sh'
 rm -rf device/xiaomi/pipa/vendorsetup.sh
