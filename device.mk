606a5778070a98b64151abdec173d7ff21bd9572
DragonEmperor9480 <amruteshnaregal1234@gmail.com>
2024-01-18 15:40:01 +0530

    Revert "pipa: drop uncessary overlays"
    This reverts commit 965b95ff54c4633649d82cd837f1b5a22ef079f2.

--- device.mk
+++ device.mk
@@ -48,7 +48,8 @@ PRODUCT_PRODUCT_PROPERTIES += \
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay
+    $(LOCAL_PATH)/overlay \
+    $(LOCAL_PATH)/overlay-lineage
 
 # Peripheral Manager
 PRODUCT_PACKAGES += \
