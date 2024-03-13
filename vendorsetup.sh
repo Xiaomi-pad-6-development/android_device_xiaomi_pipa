1c7860c55fedbd08dbb340236c73b81956dd8a9f
Amrutesh Naregal <107930548+DragonEmperor9480@users.noreply.github.com>
2024-03-13 10:03:34 +0530

    pipa: update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,5 +1,5 @@
 echo 'cloning common tree'
-git clone https://github.com/Xiaomi-pad-6-development/android_device_xiaomi_sm8250-common.git -b pos-14 device/xiaomi/sm8250-common
+git clone https://github.com/Xiaomi-pad-6-development/android_device_xiaomi_sm8250-common.git -b banana-14 device/xiaomi/sm8250-common
 
 echo 'Cloning proprietary vendor'
 git clone https://github.com/Xiaomi-pad-6-development/proprietary_vendor_xiaomi_pipa.git vendor/xiaomi/pipa
@@ -8,7 +8,7 @@ echo 'cloning common vendor'
 git clone https://github.com/Xiaomi-pad-6-development/proprietary_vendor_xiaomi_sm8250-common.git vendor/xiaomi/sm8250-common
 
 echo 'cloning kernel'
-git clone https://github.com/crdroidandroid/android_kernel_xiaomi_sm8250.git -b 14.0-pipa kernel/xiaomi/sm8250 --depth=1
+git clone https://github.com/Xiaomi-pad-6-development/kernel_xiaomi_sm8250.git kernel/xiaomi/sm8250 --depth=1
 
 echo 'cloning xiaomi hals'
 git clone https://github.com/Xiaomi-pad-6-development/android_hardware_xiaomi.git hardware/xiaomi
