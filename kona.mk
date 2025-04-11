1c63bfd4977804b8ba22d68ddde42637552b0c22
johnmart19 <johnivan19999@gmail.com>
2025-04-11 19:06:38 +0300

    pipa: Build DSPVolumeSynchronizer
    - Users reported better volume steps in BT devices

--- kona.mk
+++ kona.mk
@@ -163,7 +163,8 @@ PRODUCT_PACKAGES += \
 # Device-specific settings
 PRODUCT_PACKAGES += \
     XiaomiDolby \
-    XiaomiParts
+    XiaomiParts \
+    DSPVolumeSynchronizer
 
 # Dolby Props
 PRODUCT_VENDOR_PROPERTIES += \
