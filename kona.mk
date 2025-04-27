e33c2550c9804a4b861c5ad06fa179169d79c5d4
johnmart19 <johnivan19999@gmail.com>
2025-04-27 15:06:00 +0300

    pipa: Guard Qualcomm thermal service hals * Crashing on Alioth and some other devices
    * Crash:
     * Earlier versions:
        - E android.hardware.thermal@2.0-service.qti: ThermalHAL not initialized properly.
        - E ThermalHalWrapper: Couldn't get temperatures because of HAL error: ThermalHAL not initialized properly.
     * Current one:
        - E ThermalManagerService$ThermalHalWrapper: Couldn't getCurrentCoolingDevices due to invalid status
        - E ThermalManagerService$ThermalHalWrapper: java.lang.IllegalArgumentException: ThermalHAL not initialized properly.

--- kona.mk
+++ kona.mk
@@ -330,8 +330,10 @@ PRODUCT_SOONG_NAMESPACES += \
     vendor/qcom/opensource/usb/etc
 
 # Thermal
+ifeq ($(TARGET_USE_QTI_THERMAL_SERVICE),true)
 PRODUCT_PACKAGES += \
     android.hardware.thermal-service.qti
+endif
 
 # USB
 PRODUCT_PACKAGES += \
