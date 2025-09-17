UtsavBalar1231/platform_device_xiaomi_pipa

pipa: parts: Improve per-app refresh rate activity
Change-Id: I19018309949688b1f89900360ba6014c06da7ac7
Signed-off-by: rdxzv <rdxzv.dev@gmail.com>

parts/src/org/lineageos/settings/refreshrate/RefreshActivity.java
@@ -17,9 +17,12 @@
 package org.lineageos.settings.refreshrate;
 
 import android.os.Bundle;
-import android.preference.PreferenceActivity;
+import android.view.MenuItem;
 
-public class RefreshActivity extends PreferenceActivity {
+import com.android.settingslib.collapsingtoolbar.CollapsingToolbarBaseActivity;
+import com.android.settingslib.widget.R;
+
+public class RefreshActivity extends CollapsingToolbarBaseActivity {
 
     private static final String TAG_REFRESH = "refresh";
 
@@ -27,7 +30,17 @@ public class RefreshActivity extends PreferenceActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        getFragmentManager().beginTransaction().replace(android.R.id.content,
+        getFragmentManager().beginTransaction().replace(R.id.content_frame,
                 new RefreshSettingsFragment(), TAG_REFRESH).commit();
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            onBackPressed();
+            return true;
+        }
+        return false;
     }
 }

Xiaomi-pad-6-development/android_device_xiaomi_pipa

parts/src/org/lineageos/settings/refreshrate/RefreshActivity.java
@@ -17,9 +17,12 @@
 package org.lineageos.settings.refreshrate;
 
 import android.os.Bundle;
-import android.preference.PreferenceActivity;
+import android.view.MenuItem;
 
-public class RefreshActivity extends PreferenceActivity {
+import com.android.settingslib.collapsingtoolbar.CollapsingToolbarBaseActivity;
+import com.android.settingslib.collapsingtoolbar.R;
+
+public class RefreshActivity extends CollapsingToolbarBaseActivity {
 
     private static final String TAG_REFRESH = "refresh";
 
@@ -27,7 +30,17 @@ public class RefreshActivity extends PreferenceActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        getFragmentManager().beginTransaction().replace(android.R.id.content,
+        getFragmentManager().beginTransaction().replace(R.id.content_frame,
                 new RefreshSettingsFragment(), TAG_REFRESH).commit();
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            onBackPressed();
+            return true;
+        }
+        return false;
     }
 }